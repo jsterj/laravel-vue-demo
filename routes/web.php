3957e8ef58ddb1ed638b24e4ff8464e90b631b6d
Justin Donnell <jsterj@users.noreply.github.com>
2020-06-20 16:36:24 -0400

    Added a controller and routes for transactions
    Base controller with stubbed resource functions and CRUD routes.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return redirect('transactions');
+});
+
+Route::get('/home', function () {
+    return redirect('transactions');
+});
+
+Route::resource('transactions', 'TransactionsController')->middleware('auth');
